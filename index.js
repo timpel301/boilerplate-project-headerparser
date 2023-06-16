c04b106037fe20a2a421b196706019471f9a46b9
timpel301 <tim.pel301@gmail.com>
2023-06-16 22:02:43 +0200

    whoami endpoint definition

--- index.js
+++ index.js
@@ -24,6 +24,11 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+
+app.get('/api/whoami', (req, res) => {
+  res.json({ ipaddress: req.socket.remoteAddress, language: req.get('accept-language'), software: req.get('user-agent')});
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
